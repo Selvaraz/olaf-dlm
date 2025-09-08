68001f5f1f9189c0af4cd211e6838c3c1ea2516f
Selvaraj <selvaraz.iit@gmail.com>
2025-09-07 18:55:45 -0700

    change the pretokenzed data path

--- opal/utils/opal_constants.py
+++ opal/utils/opal_constants.py
@@ -9,10 +9,10 @@ class OpalConstants:
     CHECKPOINT_DIR = f"{RUNTIME_ROOT_PATH}/checkpoints"
     TENSORBOARD_RUN_DIR = f"{CHECKPOINT_DIR}/runs"
     CHECKPOINT_PATH = f"{CHECKPOINT_DIR}/checkpoint-latest.pt"
-    PRETOKENIZED_DATA_PATH = f"{CHECKPOINT_DIR}/pretokenized_data/"
 
     ## INPUTS
     DATA_DIR = f"{RUNTIME_ROOT_PATH}/dataset"
+    PRETOKENIZED_DATA_PATH = f"{DATA_DIR}/pretokenized_data/"
     PRETRAIN_DATA_PATH = f"{DATA_DIR}/consolidated_ascii_files"
     TOKENIZER_MODEL_PATH = f"{RUNTIME_ROOT_PATH}/tokenizer/olaf_tokenizer_09062025_45M.model"
     FINETUNE_DATA_PATH = f"{DATA_DIR}/QA_finetune_final.jsonl"
